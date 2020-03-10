2c74497fadd28f18d0eb1c3bf82feda45be540c1
ArmandoEA <armando_2496@hotmail.com>
2020-03-10 14:33:55 -0600

    Se empezó el proyecto. Se creó la clase Propiedades así como la clase encargada de la conexión a la base de datos

---
+++ tirusta_zacatecas/src/conexion/Datos_conexion.java
@@ -0,0 +1,9 @@
+package conexion;
+
+public final class Datos_conexion {
+	static final String DB_HOST = "localhost";
+	static final String DB_NOMBRE = "turista_zacatecas";
+	static final String DB_USER = "root";
+	static final String DB_PASS = "";
+	static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+}
